42e2b5638ac65f6734b85b807d94b8051d63896e
Nicolas DEVAUX <ndevaux@gmail.com>
2026-01-23 17:34:27 +0100

    .

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'RE-FLOW - Programme de reconnexion corporelle';
+export const SITE_DESCRIPTION = 'Quand le corps redevient un allié. Programme de reconnexion corporelle pour les femmes touchées par une maladie chronique.';
