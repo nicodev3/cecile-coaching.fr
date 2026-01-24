02c44a76df1221262d87cb0a0177147e5e1e0426
nicodev <ndevaux@gmail.com>
2026-01-24 09:48:32 +0100

    .

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'RE-FLOW - Programme de reconnexion corporelle';
-export const SITE_DESCRIPTION = 'Quand le corps redevient un allié. Programme de reconnexion corporelle pour les femmes touchées par une maladie chronique.';
+export const SITE_DESCRIPTION = 'Quand le corps redevient un allié. Programme de reconnexion corporelle pour les femmes touchées par une maladie chronique. Coach certifiée APA — Activités Physiques Adaptées.';
