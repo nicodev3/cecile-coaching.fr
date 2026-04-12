f6f630e4badccea590c39197d388e2d805d55fe9
Nicolas DEVAUX <ndevaux@gmail.com>
2026-04-12 20:25:34 +0200

    Update site URL in configuration and components to remove hyphen for consistency. Adjust sitemap link in robots.txt accordingly.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cecile-coaching.fr',
+  site: 'https://cecilecoaching.fr',
   integrations: [mdx(), sitemap()],
 
   fonts: [
