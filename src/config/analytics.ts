82545ddba74501a00e9127f0c786dc1253501913
Nicolas DEVAUX <nicolas.devaux@sciencespo.fr>
2026-06-25 15:54:40 +0200

    .

---
+++ src/config/analytics.ts
@@ -0,0 +1,4 @@
+export const AHREFS_ANALYTICS_KEY = "7fW1UK+MbVXhQ9ADKKYHWw";
+
+export const UMAMI_WEBSITE_ID = "5b381271-da61-48cc-b718-d4595151d016";
+export const UMAMI_URL = "https://analytics.nicodev.fr";
